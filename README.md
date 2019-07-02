90c74ae5fee8825326af0fcb1573a4bc94372cf3
john <john@3kd.be>
2019-07-02 16:37:32 +0200

    working webhook for blink identity

---
+++ README.md
@@ -0,0 +1,13 @@
+# Yo.
+
+Example data response from webhook :
+
+```json
+{ 
+  StationGuid: '8b75544b-7750-4311-afd2-0d4945f7420b',
+  ContainsBiometrics: true,
+  PersonName: 'Jo',
+  PersonGuid: '86aae7b5-6911-443a-abe7-0cca8e3a8918',
+  PersonState: 0
+}
+```
--- src/api-consumer.js
+++ src/api-consumer.js
@@ -1,11 +1,53 @@
-class ApiConsumer  {
+const ngrok = require('ngrok');
+const axios = require('axios');
+// const connect = require('connect');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
+
+// const http = require('http');
+// const app = connect();
+
+class ApiConsumer {
   constructor(callback) {
     this._callback = callback;
-    console.log(process.env.BASE_URL);
+    this.addWebhook();
+
+
+    app.use('/', (req, res) => {
+      this._callback(req.body);
+      console.log('Webhook called !', req.body);
+      res.send('🦄');
+    });
+
+    app.listen(process.env.HTTP_PORT, () => {
+      console.log(`Server running on ${process.env.HTTP_PORT}...`);
+    });
   }
 
-  onMatch(match) {
-    this._callback(match);
+  async addWebhook() {
+    const ngrok_url = await ngrok.connect(process.env.HTTP_PORT);
+    console.log(`HTTP tunnel on ${ngrok_url}`);
+
+    try {
+      const api_url = `${process.env.BASE_URL}Matcher/AddMatchWebhook`;
+      await axios.post(api_url, {
+        matchWebhook: ngrok_url
+      }, {
+        params: {
+          matcherGuid: process.env.MATCHER_GUID,
+        },
+        headers: {
+          'Authorization': `Bearer ${process.env.JWT_TOKEN}`,
+          'Content-Type': 'application/json'
+        }
+      });
+
+      console.log('Webhook added 👍');
+    } catch (e) {
+      console.error(e);
+    }
   }
 }
 
